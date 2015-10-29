6b363fbadb89e28df9a8d8b64d8564cd6911f087
Alessio Carrafa <alessio.carrafa@hotmail.it>
2015-10-29 18:08:16 +0100

    added resource rest call, added external redirection

--- api.php
+++ api.php
@@ -14,9 +14,9 @@ if( FALSE !== ( $handle = opendir( OBJ_DIR ) ) )
 	{
 		if( $entry != "." && $entry != ".." && strtolower( substr( $entry, strrpos( $entry, '.' ) + 1) ) == 'json' )
 		{
-			//echo "$entry\n";
-			$string				= file_get_contents( OBJ_DIR . $entry );
-			$readed[ $entry ]	= json_decode( $string, true );
+			$string = file_get_contents( OBJ_DIR . $entry );
+			$decoded = json_decode( $string, true );
+			$readed[ $decoded['name'] ]	= $decoded;
 		}
 	}
 	
@@ -36,10 +36,49 @@ foreach( $readed as $key => &$obj_file )
 		echo( json_encode( [ "resource" => "/" . $obj_file['name'] ] ) );
 	});
 	
+	$app->get( "/" . $obj_file['name'] . "/:id", function( $id ) use ( $app, $obj_file )
+	{
+		$key_name = GetPrimaryKey( $obj_file['data'] );
+		echo( json_encode( [ "resource" => "Search for PRIMARY KEY /" . $obj_file['name'] . "/$id with keyname of $key_name" ] ) );
+	});
+	
 	$app->get( "/" . $obj_file['name_plural'], function() use ( $app, $obj_file )
 	{
 		echo( json_encode( [ "resource" => "/" . $obj_file['name_plural'] ] ) );
 	});
+	
+	foreach( $obj_file['data'] as $data_key => &$data_value )
+	{
+		$app->get( "/" . $obj_file['name'] . "/" . $data_key . "/:value", function( $value ) use ( $app, $data_key, $data_value )
+		{
+			$type			= $data_value['type'];
+			$casted_value	= NULL;
+
+				 if( $type == "int"		)	$casted_value = intval( $value );
+			else if( $type == "string"	)	$casted_value = (string)( $value );
+			else							$casted_value = $value;
+			
+			$external = $data_value['ext'];
+
+			$str = "Search for $data_key with value of $casted_value typed like [ $type ]";
+			
+			if( $external ) $str .= ", with external referer in " . $external['ext_resource'];
+
+			echo( json_encode( [ "resource" => $str ] ) );
+			
+			if( $external ) $app->response->redirect( "/" . $external['ext_resource'] . "/" . $casted_value );
+		});
+	}
+	
 }
 
 $app->run();
+
+function GetPrimaryKey( $data )
+{
+	foreach( $data as $key => &$val )
+	{
+		if( $val['key'] ) return $key;
+	}
+	return NULL;
+}
